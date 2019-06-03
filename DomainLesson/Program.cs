9953fc3a1ecc1a2f45313a9883d3dd6b06301e90
Alisher Kalabaev <alisher.alish333@gmail.com>
2019-06-03 20:14:29 +0600

    DomainLesson

--- DomainLesson/Program.cs
+++ DomainLesson/Program.cs
@@ -10,6 +10,29 @@ namespace DomainLesson
     {
         static void Main(string[] args)
         {
+            var currentDomain = AppDomain.CurrentDomain;
+            AnalizeDomain(currentDomain);
+
+            var secondDomain = AppDomain.CreateDomain("Another Specific Domain Area");
+            AnalizeDomain(secondDomain);
+            secondDomain.ExecuteAssembly(secondDomain.BaseDirectory + "Test2.exe", new string[] { "5", "7" });
+            AnalizeDomain(secondDomain);
+
+            AppDomain.Unload(secondDomain);
+
+            Console.ReadLine();
+        }
+
+        private static void AnalizeDomain(AppDomain currentDomain)
+        {
+            Console.WriteLine($"***{currentDomain.FriendlyName}, {currentDomain.BaseDirectory}, {currentDomain.Id}");
+
+            var assemblies = currentDomain.GetAssemblies();
+
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine($"{assembly.FullName}, {assembly.Location}");
+            }
         }
     }
 }
